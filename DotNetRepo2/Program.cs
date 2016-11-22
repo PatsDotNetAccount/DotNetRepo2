cfc375def90ee4e88255a28ddeb8b0b090689fe1
Pat <Pat@microsoft.com>
2016-11-22 20:10:42 +0100

    Initial commit 3

---
+++ DotNetRepo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetRepo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Git-Welt 3");
+            Console.ReadKey();
+        }
+    }
+}
