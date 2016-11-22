a5231dbcc9ff3af0b633bb8eaf7ba3cec9f3dcc7
Pat <Pat@microsoft.com>
2016-11-22 21:46:55 +0100

    Reformatting

--- DotNetRepo2/Program.cs
+++ DotNetRepo2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNetRepo2
 {
@@ -15,7 +11,7 @@ namespace DotNetRepo2
             Console.WriteLine("Erste Änderung...");
 
 
-Console.WriteLine("Erste Änderung vom Tablet aus...");
+            Console.WriteLine("Erste Änderung vom Tablet aus...");
 
             Console.ReadKey();
         }
